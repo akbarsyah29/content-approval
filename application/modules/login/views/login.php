ef453e48f57eb2552101026da9d12b968046fa41
RezaPahlevi <chester_phalevi@windowslive.com>
2017-02-01 22:39:54 +0700

    Fixed typos & asset files

--- application/modules/login/views/login.php
+++ application/modules/login/views/login.php
@@ -25,31 +25,32 @@ License: You must have a valid license purchased only from themeforest(the above
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="Preview page of Metronic Admin Theme #4 for " name="description" />
-        <meta content="" name="author" />
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
         <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
         <?php echo css_asset('font-awesome.min.css') ?>
-        <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+        <?php echo css_asset('simple-line-icons.min.css') ?>
+        <?php echo css_asset('bootstrap.min.css') ?>
+        <?php echo css_asset('bootstrap-switch.min.css') ?>
         <!-- END GLOBAL MANDATORY STYLES -->
         <!-- BEGIN PAGE LEVEL PLUGINS -->
-        <link href="../assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <?php echo css_asset('select2.min.css') ?>
+        <?php echo css_asset('select2-bootstrap.min.css') ?>
         <!-- END PAGE LEVEL PLUGINS -->
         <!-- BEGIN THEME GLOBAL STYLES -->
-        <link href="../assets/global/css/components-rounded.min.css" rel="stylesheet" id="style_components" type="text/css" />
-        <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+        <?php echo css_asset('components-rounded.min.css') ?>
+        <?php echo css_asset('plugins.min.css') ?>
+        <?php echo css_asset('login-4.min.css') ?>
         <!-- END THEME GLOBAL STYLES -->
         <!-- BEGIN PAGE LEVEL STYLES -->
-        <link href="../assets/pages/css/login-4.min.css" rel="stylesheet" type="text/css" />
         <!-- END PAGE LEVEL STYLES -->
         <!-- BEGIN THEME LAYOUT STYLES -->
         <!-- END THEME LAYOUT STYLES -->
-        <link rel="shortcut icon" href="favicon.ico" /> </head>
+        <link rel="shortcut icon" href="favicon.ico" /> 
+    </head>
     <!-- END HEAD -->
 
     <body class=" login">
+    	<?php //echo image_asset('coba.jpg', '', 'jpg') ?>
         <!-- BEGIN LOGO -->
         <div class="logo">
             <a href="index.html">
@@ -445,24 +446,25 @@ License: You must have a valid license purchased only from themeforest(the above
 <script src="../assets/global/plugins/ie8.fix.min.js"></script> 
 <![endif]-->
         <!-- BEGIN CORE PLUGINS -->
-        <script src="../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+        <?php echo js_asset('jquery.min.js') ?>
+        <?php echo js_asset('bootstrap.min.js') ?>
+        <?php echo js_asset('js.cookie.min.js') ?>
+        <?php echo js_asset('jquery.slimscroll.min.js') ?>
+        <?php echo js_asset('jquery.blockui.min.js') ?>
+        <?php echo js_asset('bootstrap-switch.min.js') ?>
         <!-- END CORE PLUGINS -->
         <!-- BEGIN PAGE LEVEL PLUGINS -->
-        <script src="../assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/backstretch/jquery.backstretch.min.js" type="text/javascript"></script>
+        <?php echo js_asset('jquery.validate.min.js') ?>
+        <?php echo js_asset('additional-methods.min.js') ?>
+        <?php echo js_asset('select2.full.min.js') ?>
+        <?php echo js_asset('jquery.backstretch.min.js') ?>
+
         <!-- END PAGE LEVEL PLUGINS -->
         <!-- BEGIN THEME GLOBAL SCRIPTS -->
-        <script src="../assets/global/scripts/app.min.js" type="text/javascript"></script>
+        <?php echo js_asset('app.min.js') ?>
         <!-- END THEME GLOBAL SCRIPTS -->
         <!-- BEGIN PAGE LEVEL SCRIPTS -->
-        <script src="../assets/pages/scripts/login-4.min.js" type="text/javascript"></script>
+        <?php echo js_asset('login-4.min.js') ?>
         <!-- END PAGE LEVEL SCRIPTS -->
         <!-- BEGIN THEME LAYOUT SCRIPTS -->
         <!-- END THEME LAYOUT SCRIPTS -->
